acecdd6f461157a3e716ee61e09114adfa058e2b
yassuhacks750 <yassudj8085231272@gmail.com>
2025-06-12 14:28:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,16 +63,15 @@ BUTTONSCONTACT = InlineKeyboardMarkup([[InlineKeyboardButton(text="📞 Contact"
 keyboard = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="🛠️ Help", url="https://t.me/+3k-1zcJxINYwNGZl"),
-            InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/nikhilsainiop/saini-txt-direct"),
+            InlineKeyboardButton(text="🛠️ Help", url="https://ik.imagekit.io/pqmtwh2fn/file_00000000995061f8bb61d5e0580022ba.png"),
+            InlineKeyboardButton(text="🛠️ Repo", url="https://ik.imagekit.io/pqmtwh2fn/file_00000000995061f8bb61d5e0580022ba.png"),
         ],
     ]
 )
 
 # Image URLs for the random image feature
 image_urls = [
-    "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg",
-    "https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png",
+    "https://ik.imagekit.io/pqmtwh2fn/file_00000000995061f8bb61d5e0580022ba.png",
     # Add more image URLs as needed
 ]
 
